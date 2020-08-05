2f4b378beb1fc6110e218903a659e2d00801832a
Bruce Baron <baron.br@gmail.com>
2020-08-05 14:26:31 -0400

    added config project

--- orders/src/main/java/app/OrderController.java
+++ orders/src/main/java/app/OrderController.java
@@ -1,17 +1,38 @@
 package app;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/api/orders")
+@RefreshScope
 public class OrderController {
+
+    private final String applicationMessage;
+    private final String orderMessage;
+
+    public OrderController(
+            @Value("${message:No message}")
+            String applicationMessage,
+            @Value("${order-message:No message}")
+            String orderMessage) {
+        this.applicationMessage = applicationMessage;
+        this.orderMessage = orderMessage;
+    }
 
     @GetMapping
     List<Order> findAll() {
         return List.of();
+    }
+
+    @GetMapping("/messages")
+    Map<String, String> getMessages() {
+        return Map.of("message", applicationMessage, "order-message", orderMessage);
     }
 }
