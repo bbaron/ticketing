ae0e4eb11114225db8bc805ea8ca291a8756f48c
Bruce Baron <baron.br@gmail.com>
2020-08-14 15:30:09 -0400

    add starter

---
+++ ticketing-spring-boot-starter/src/main/java/ticketing/TicketingSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingSpringBootStarterApplication.class, args);
+    }
+
+}
