b63c2a823e699509fbf23445b6783e2db3904970
Bruce Baron <baron.br@gmail.com>
2020-08-15 16:44:47 -0400

    added OrderStatus

---
+++ ticketing-spring-boot-starter/src/main/java/ticketing/events/types/OrderStatus.java
@@ -0,0 +1,20 @@
+package ticketing.events.types;
+
+public enum OrderStatus {
+    // When the order has been created, but the
+    // ticket it is trying to order has not been reserved
+    Created,
+
+    // The ticket is order is trying to reserve has already
+    // been reserved, or when the user has cancelled the order
+    // The order expires before payment
+    Cancelled,
+
+    // The order has successfully reserved the ticket
+    AwaitingPayment,
+
+    // The order has reserved the ticket and the user has
+    // provided payment successfully
+    Complete,
+
+}
