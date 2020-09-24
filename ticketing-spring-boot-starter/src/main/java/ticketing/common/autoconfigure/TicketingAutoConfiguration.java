c2a6744fae637af87fbecf53be2a029a46469b95
Bruce Baron <baron.br@gmail.com>
2020-09-24 13:54:27 -0400

    removed old event code

--- ticketing-spring-boot-starter/src/main/java/ticketing/common/autoconfigure/TicketingAutoConfiguration.java
+++ ticketing-spring-boot-starter/src/main/java/ticketing/common/autoconfigure/TicketingAutoConfiguration.java
@@ -3,13 +3,12 @@ package ticketing.common.autoconfigure;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import ticketing.common.autoconfigure.events.EventsAutoConfiguration;
 import ticketing.common.autoconfigure.json.JsonAutoConfiguration;
 import ticketing.common.autoconfigure.security.ResourceServerAutoConfiguration;
 import ticketing.common.autoconfigure.web.WebAutoConfiguration;
 
 @Configuration
-@Import({WebAutoConfiguration.class, EventsAutoConfiguration.class, JsonAutoConfiguration.class, ResourceServerAutoConfiguration.class})
+@Import({WebAutoConfiguration.class, JsonAutoConfiguration.class, ResourceServerAutoConfiguration.class})
 @EnableConfigurationProperties(TicketingProperties.class)
 public class TicketingAutoConfiguration {
 }
--- ticketing-spring-boot-starter/src/main/java/ticketing/common/events/Publisher.java
+++
@@ -1,8 +0,0 @@
-package ticketing.common.events;
-
-@Deprecated
-public interface Publisher<E extends Event> {
-    Subject subject();
-
-    void publish(E event);
-}
